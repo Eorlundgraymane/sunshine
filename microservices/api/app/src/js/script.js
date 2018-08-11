b0b0f9b1283914b86ed9337e3e86c1a38026b5e8
Eorlundgraymane <rkmenon235@gmail.com>
2018-08-12 00:46:46 +0530

    DB Patch  Up

--- microservices/api/app/src/js/script.js
+++ microservices/api/app/src/js/script.js
@@ -480,7 +480,7 @@ newsApp.controller("newsCtrl",function($scope,$http){
     var file = document.getElementById("newpostimg").files[0];
     var newimg  = $scope.newpostimg;
     var date = new Date();
-    var dpurl = "https://filestore.unluckily34.hasura-app.io/v1/file/"+JSON.parse(getCookie("userdata"))[0].id+"-"+date.getTime();
+    var dpurl = "http://filestore.unluckily34.hasura-app.io/v1/hooks/public-read-user-write/"+JSON.parse(getCookie("userdata"))[0].id+"-"+date.getTime();
       ////console.log(dpurl);
       $http({
         method : "POST",
@@ -2046,7 +2046,7 @@ muserProfileApp.controller("muserProfileCtrl",function($scope,$http){
     proggy(30+(i/1000));
   }
   var date = new Date();
-  var dpurl = "  http://filestore.your-project-domain/v1/hooks/public-read-user-write/"+JSON.parse(getCookie("userdata"))[0].id+"-"+date.getTime();
+  var dpurl = "  http://filestore.unluckily34.hasura-app.io/v1/hooks/public-read-user-write/"+JSON.parse(getCookie("userdata"))[0].id+"-"+date.getTime();
     ////console.log(dpurl);
     $http({
       method : "POST",
@@ -2441,7 +2441,7 @@ userProfileApp.controller("userProfileCtrl",function($scope,$http){
   $scope.uploadDP = function(file){
     sunNotify("<i class = 'fa fa-spinner fa-spin'></i> <strong>Uploading...</strong>","alert-warning");
     var date = new Date();
-    var dpurl = "https://filestore.unluckily34.hasura-app.io/v1/file/"+JSON.parse(getCookie("userdata"))[0].id+"-"+date.getTime();
+    var dpurl = "http://filestore.unluckily34.hasura-app.io/v1/hooks/public-read-user-write/"+JSON.parse(getCookie("userdata"))[0].id+"-"+date.getTime();
       ////console.log(dpurl);
       $http({
         method : "POST",
