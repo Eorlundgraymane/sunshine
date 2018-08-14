3876bfe908d1614e2bbe80856f0e4ae2326d01e2
Eorlundgraymane <rkmenon235@gmail.com>
2018-08-14 14:27:20 +0530

    SHine bars

--- microservices/api/app/src/content.php
+++ microservices/api/app/src/content.php
@@ -45,7 +45,7 @@
       </div>
       <div id = "shinetable" ng-app = "shineTableApp" ng-controller = "shineTableCtrl" class = "col-xs-0 col-md-3 col-lg-3 col-sm-3">
         <div class = "row">
-          <div class = "bordered col-xs-8 col-md-8 col-lg-8 col-sm-8">
+          <div class = "bordered col-xs-10 col-md-10 col-lg-10 col-sm-10">
             <table class = "table shinetable" id = "table">
               <thead><caption><strong>Shine Score</strong></caption></thead>
               <tr class = "row">
@@ -53,7 +53,7 @@
                 <td  class = "col-xs-0 col-md-11 col-lg-11 col-sm-11">
                   <div class = "row">
                     <div class = "container-fluid col-xs-12 col-md-12 col-sm-12 col-lg-12 ">
-                      <div id = "healthprog" class="progress-bar progress-bar-danger active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                      <div id = "healthprog" class="shineprog progress-bar progress-bar-danger active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                   </div>
                 </td>
@@ -63,7 +63,7 @@
                 <td  class = "col-xs-0 col-md-11 col-lg-11 col-sm-11">
                   <div class = "row">
                     <div class = "container-fluid col-xs-12 col-md-12 col-sm-12 col-lg-12 ">
-                      <div id = "earthprog" class="progress-bar progress-bar-success active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                      <div id = "earthprog" class="shineprog progress-bar progress-bar-success active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                   </div>
                 </td>
@@ -73,7 +73,7 @@
                 <td  class = "col-xs-0 col-md-11 col-lg-11 col-sm-11">
                   <div class = "row">
                     <div class = "container-fluid col-xs-12 col-md-12 col-sm-12 col-lg-12 ">
-                      <div id = "charityprog" class="progress-bar progress-bar-warning active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                      <div id = "charityprog" class="shineprog progress-bar progress-bar-warning active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                   </div>
                 </td>
@@ -83,14 +83,14 @@
                 <td  class = "col-xs-0 col-md-11 col-lg-11 col-sm-11">
                   <div class = "row">
                     <div class = "container-fluid col-xs-12 col-md-12 col-sm-12 col-lg-12 ">
-                      <div id = "socialprog" class="progress-bar progress-bar-primary active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                      <div id = "socialprog" class="shineprog progress-bar progress-bar-primary active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
                   </div>
                 </td>
               </tr>
             </table>
           </div>
-          <div id = "giftboxdiv" class = "bordered col-xs-0 col-md-4 col-lg-4 col-sm-4">
+          <div id = "giftboxdiv" class = "bordered col-xs-0 col-md-2 col-lg-2 col-sm-2">
             <ul id = "giftbox">
             </ul>
           </div>
@@ -157,25 +157,25 @@
           <tr class = "row">
             <th class = "col-xs-1 col-md-1 col-lg-0 col-sm-1"><span class = "label shinelabel label-danger">{{hshine}}</span></th>
             <td  class = "col-xs-11 col-md-11 col-lg-0 col-sm-11">
-              <div id = "hshine" class="progress-bar progress-bar-danger active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              <div id = "hshine" class="shineprog progress-bar progress-bar-danger active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
             </td>
           </tr>
           <tr class = "row">
             <th class = "col-xs-1 col-md-1 col-lg-0 col-sm-1"><span class = "label shinelabel label-success">{{eshine}}</span></th>
             <td  class = "col-xs-11 col-md-11 col-lg-0 col-sm-11">
-              <div id = "eshine" class="progress-bar progress-bar-success active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              <div id = "eshine" class="shineprog progress-bar progress-bar-success active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
             </td>
           </tr>
           <tr class = "row">
             <th class = "col-xs-1 col-md-1 col-lg-0 col-sm-1"><span class = "label shinelabel label-warning">{{cshine}}</span></th>
             <td  class = "col-xs-11 col-md-11 col-lg-0 col-sm-11">
-              <div id = "cshine" class="progress-bar progress-bar-warning active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              <div id = "cshine" class="shineprog progress-bar progress-bar-warning active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
             </td>
           </tr>
           <tr class = "row">
             <th class = "col-xs-1 col-md-1 col-lg-0 col-sm-1"><span class = "label shinelabel label-primary">{{sshine}}</span></th>
             <td class = "col-xs-11 col-md-11 col-lg-0 col-sm-11">
-              <div id = "sshine" class="progress-bar progress-bar-primary active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+              <div id = "sshine" class="shineprog progress-bar progress-bar-primary active" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
             </td>
           </tr>
         </table>
